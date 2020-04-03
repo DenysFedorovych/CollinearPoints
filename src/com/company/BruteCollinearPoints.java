6ccf655ce0093c55ba50bf163aa634fd1699842f
DenysFedorovych <fedorovich.d.d@gmail.com>
2020-04-03 15:46:45 +0300

    Third class, nearly produced

--- src/com/company/BruteCollinearPoints.java
+++ src/com/company/BruteCollinearPoints.java
@@ -1,19 +1,41 @@
 package com.company;
 
 public class BruteCollinearPoints {
-    public BruteCollinearPoints (Point [] points)  {
-        for (int j = 0; j < points.length - 4; j++) {
-            double sum = 0;
-            for (int i = j; i < j + 4; i++) {
-                double p = points[i].slopeTo(points[i + 1]);
-                sum += p;
-            }
-            if (sum/4 == points[j].slopeTo(points[j + 1])){
-                
+    private int k=0;
+    private LineSegments[2] segments;
+    public BruteCollinearPoints(Point[] points){
+        int m = points.length;
+        for (int p=0; p<m; p++)
+        {
+            for(int g=p+1; g<m; g++)
+            {
+                if(points[p]==points[g] || points[p]==null || points[g]==null)
+                {
+                    throw new IllegalArgumentException("Wrong Argument");
+                }
             }
         }
-    }   // находит все сегменты линии, содержащие 4 точки
-    public int numberOfSegments ()         // количество сегментов линии
-    public LineSegment [] segments ()                 // сегменты линии
-}
+        points.sort();
+        for (int i=0; i<m; i++)
+        {
+            for(int j=i+1; j<m; m++)
+            {
+                for(int l=j+1;l<m;l++)
+                {
+                    for(int h=l+1; h<m; h++)
+                    {
+                         if(points[h].slopeTo(points[l])==points[l].slopeTo(points[j]) && points[l].slopeTo(points[j])==points[j].slopeTo(points[i]))
+                         {
+                             segments[k] = LineSegment(points[i],points[h]);
+                             k++;
+                         }
+                    }
+                }
+            }
+        }
+    }    // finds all line segments containing 4 points
 
+    public           int numberOfSegments(){return k;}        // the number of line segments
+
+    public LineSegment[] segments()                // the line segments
+}
